47a50b7182c3785150403670c58b419469b0d1c1
mateusgomst <mateusgomes9119@gmail.com>
2025-04-22 13:18:20 -0300

    corrige problemas de encoding e configuração do Docker

--- src/main/java/com/prime_cell/back_end/infra/security/SecurityConfig.java
+++ src/main/java/com/prime_cell/back_end/infra/security/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.GET, "/products").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/auth/register").denyAll()
+                        .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                         .requestMatchers(HttpMethod.POST, "/products").authenticated()
                         .requestMatchers(HttpMethod.PUT, "/products").authenticated()
                         .requestMatchers(HttpMethod.DELETE, "/products").authenticated()
